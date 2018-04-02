6fd0304cad4e666bee6246cc550f74a4778824e7
陈弘桂 <chg80333@163.com>
2018-04-02 17:19:38 +0800

    initial

--- filters/auth.go
+++ filters/auth.go
@@ -1,1 +1,19 @@
 package filters
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("X-Auth-Token")
+		appid := c.Request.Header.Get("X-Auth-Appid")
+
+		if token == "" && appid == "" {
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
