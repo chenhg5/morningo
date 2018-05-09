5addcf1dfe30558909f5f6656b142c71eceab425
陈弘桂 <chg80333@163.com>
2018-05-09 14:36:45 +0800

    fix mysql bug

--- connections/database/mysql/mysql.go
+++ connections/database/mysql/mysql.go
@@ -59,10 +59,62 @@ func Query(query string, args ...interface{}) ([]map[string]interface{}, *sql.Ro
 		var colVar = make([]interface{}, len(col))
 		for i := 0; i < len(col); i++ {
 			// Tips: string类型如果为interface返回乱字符串
-			if typeVal[i].ScanType().Name() == "RawBytes" {
+			switch typeVal[i].DatabaseTypeName() {
+			case "INT":
+				var s int
+				colVar[i] = &s
+			case "TINYINT":
+				var s int
+				colVar[i] = &s
+			case "MEDIUMINT":
+				var s int
+				colVar[i] = &s
+			case "SMALLINT":
+				var s int
+				colVar[i] = &s
+			case "BIGINT":
+				var s int
+				colVar[i] = &s
+			case "FLOAT":
+				var s float32
+				colVar[i] = &s
+			case "DOUBLE":
+				var s float32
+				colVar[i] = &s
+			case "DECIMAL":
+				var s int
+				colVar[i] = &s
+			case "DATE":
 				var s string
 				colVar[i] = &s
-			} else {
+			case "TIME":
+				var s string
+				colVar[i] = &s
+			case "YEAR":
+				var s string
+				colVar[i] = &s
+			case "DATETIME":
+				var s string
+				colVar[i] = &s
+			case "TIMESTAMP":
+				var s string
+				colVar[i] = &s
+			case "VARCHAR":
+				var s string
+				colVar[i] = &s
+			case "MEDIUMTEXT":
+				var s string
+				colVar[i] = &s
+			case "LONGTEXT":
+				var s string
+				colVar[i] = &s
+			case "TINYTEXT":
+				var s string
+				colVar[i] = &s
+			case "TEXT":
+				var s string
+				colVar[i] = &s
+			default:
 				var s interface{}
 				colVar[i] = &s
 			}
@@ -73,7 +125,46 @@ func Query(query string, args ...interface{}) ([]map[string]interface{}, *sql.Ro
 			panic(scanErr)
 		}
 		for j := 0; j < len(col); j++ {
-			result[col[j]] = *(colVar[j].(*string))
+			switch typeVal[j].DatabaseTypeName() {
+			case "INT":
+				result[col[j]] = *(colVar[j].(*int))
+			case "TINYINT":
+				result[col[j]] = *(colVar[j].(*int))
+			case "MEDIUMINT":
+				result[col[j]] = *(colVar[j].(*int))
+			case "SMALLINT":
+				result[col[j]] = *(colVar[j].(*int))
+			case "BIGINT":
+				result[col[j]] = *(colVar[j].(*int))
+			case "FLOAT":
+				result[col[j]] = *(colVar[j].(*float32))
+			case "DOUBLE":
+				result[col[j]] = *(colVar[j].(*float32))
+			case "DECIMAL":
+				result[col[j]] = *(colVar[j].(*int))
+			case "DATE":
+				result[col[j]] = *(colVar[j].(*string))
+			case "TIME":
+				result[col[j]] = *(colVar[j].(*string))
+			case "YEAR":
+				result[col[j]] = *(colVar[j].(*string))
+			case "DATETIME":
+				result[col[j]] = *(colVar[j].(*string))
+			case "TIMESTAMP":
+				result[col[j]] = *(colVar[j].(*string))
+			case "VARCHAR":
+				result[col[j]] = *(colVar[j].(*string))
+			case "MEDIUMTEXT":
+				result[col[j]] = *(colVar[j].(*string))
+			case "LONGTEXT":
+				result[col[j]] = *(colVar[j].(*string))
+			case "TINYTEXT":
+				result[col[j]] = *(colVar[j].(*string))
+			case "TEXT":
+				result[col[j]] = *(colVar[j].(*string))
+			default:
+				result[col[j]] = colVar[j]
+			}
 		}
 		results = append(results, result)
 	}
